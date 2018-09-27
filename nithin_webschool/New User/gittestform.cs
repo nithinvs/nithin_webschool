93f38e165f5f5c7fe963a559202b5f6ff2fa9d81
nithinvs <nithin.shivamangalam@gmail.com>
2018-09-28 01:24:44 +0530

    new useril test form and pinnem testform

---
+++ nithin_webschool/New User/gittestform.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nithin_webschool.New_User
+{
+    public partial class gittestform : Form
+    {
+        public gittestform()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ nithin_webschool/veruthe testForm2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nithin_webschool
+{
+    public partial class veruthe_testForm2 : Form
+    {
+        public veruthe_testForm2()
+        {
+            InitializeComponent();
+        }
+    }
+}
